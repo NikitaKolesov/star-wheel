b35729e14872a67cf079e1a1fc1c43486f571cd7
Nikita Kolesov <nikita.kolesov@acronis.com>
2019-10-26 18:57:08 +0300

    telegram works

--- tests/const.py
+++ tests/const.py
@@ -11,6 +11,15 @@ AUTH_DATA = {
     "auth_date": 1565899537,
     "hash": "3ee3d6b0617dad1b0d42a38126af399e679b8656e2872087e56c83d36c0b45a9",
 }
+AUTH_DATA_K = {
+    "id": 269813866,
+    "first_name": "Екатерина",
+    "last_name": "К",
+    "username": "wheelov",
+    "photo_url": "https://t.me/i/userpic/320/y4wgSrKaPPKAeSAHOX_R7aNQDbZ8xCnVawLGPA5eI_c.jpg",
+    "auth_date": 1571776362,
+    "hash": "88ad3d4bb55de1202360e9354e63165bd09063decdbee85a565861d8981efb0e",
+}
 DEFAULT_PASSWORD = "qwe123QWE"
 fake_users_dicts = [
     {"login": "admin", "password": DEFAULT_PASSWORD},
